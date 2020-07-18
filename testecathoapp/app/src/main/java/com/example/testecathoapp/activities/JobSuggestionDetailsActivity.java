e98af4cb008db69ac9e2ea57ea53559382c6dea9
miguelfreitas18 <miguelfreitas8000@gmail.com>
2020-07-18 13:54:04 -0300

    Layout da Activity com detalhes de uma vaga (feature extra)

--- testecathoapp/app/src/main/java/com/example/testecathoapp/activities/JobSuggestionDetailsActivity.java
+++ testecathoapp/app/src/main/java/com/example/testecathoapp/activities/JobSuggestionDetailsActivity.java
@@ -3,7 +3,7 @@ package com.example.testecathoapp.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 /*
- *  Activity com detalhes de uma sugestão de vaga visualizada
+ *  Activity com detalhes de uma sugestão de vaga visualizada (feature extra)
  */
 public class JobSuggestionDetailsActivity extends AppCompatActivity
 {
